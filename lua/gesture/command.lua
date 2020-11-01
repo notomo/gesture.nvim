f9416951c0fe957d8e41279d2b810544a146f4a1
notomo <notomo.motono@gmail.com>
2020-11-01 10:21:06 +0900

    Refactor gesture/command

--- lua/gesture/command.lua
+++ lua/gesture/command.lua
@@ -3,76 +3,57 @@ local states = require("gesture/state")
 
 local M = {}
 
-local cmds = {
-  draw = function(_)
-    local state = states.get_or_create()
-    local valid = state:update()
-    if not valid then
-      return
-    end
+local GesturePlugin = {}
 
-    local inputs = state.inputs
-    local bufnr = state.source_bufnr
-    local nowait_gesture = mapper.nowait_match(bufnr, inputs)
-    if nowait_gesture ~= nil then
-      state.view:close()
-      return nowait_gesture.execute()
-    end
-
-    local gesture = mapper.match(bufnr, inputs)
-    local has_forward_match = mapper.has_forward_match(bufnr, inputs)
-    state.view:render_input(inputs, gesture, has_forward_match)
-  end,
-  finish = function(_)
-    local state = states.get()
-    if state == nil then
-      return
-    end
-    state.view:close()
-
-    local gesture = mapper.match(state.source_bufnr, state.inputs)
-    if gesture ~= nil then
-      return gesture.execute()
-    end
-  end,
-  cancel = function(_)
-    local state = states.get()
-    if state == nil then
-      return
-    end
-    state.view:close()
-  end,
-}
-
-M.parse_args = function(raw_args)
-  args = {cmd = "draw"}
-
-  if #raw_args == 0 then
-    return args
+function GesturePlugin.draw()
+  local state = states.get_or_create()
+  local valid = state:update()
+  if not valid then
+    return
   end
 
-  for _, raw_arg in ipairs(raw_args) do
-    if not vim.startswith(raw_arg, "--") then
-      args.cmd = raw_arg
-      break
-    end
+  local inputs = state.inputs
+  local bufnr = state.source_bufnr
+  local nowait_gesture = mapper.nowait_match(bufnr, inputs)
+  if nowait_gesture ~= nil then
+    state.view:close()
+    return nowait_gesture.execute()
   end
 
-  return args
+  local gesture = mapper.match(bufnr, inputs)
+  local has_forward_match = mapper.has_forward_match(bufnr, inputs)
+  state.view:render_input(inputs, gesture, has_forward_match)
+end
+
+function GesturePlugin.finish()
+  local state = states.get()
+  if state == nil then
+    return
+  end
+  state.view:close()
+
+  local gesture = mapper.match(state.source_bufnr, state.inputs)
+  if gesture ~= nil then
+    return gesture.execute()
+  end
+end
+
+function GesturePlugin.cancel()
+  local state = states.get()
+  if state == nil then
+    return
+  end
+  state.view:close()
 end
 
 M.main = function(...)
-  local args = M.parse_args({...})
-
-  local cmd = cmds[args.cmd]
+  local cmd_name = ({...})[1] or "draw"
+  local cmd = GesturePlugin[cmd_name]
   if cmd == nil then
-    return vim.api.nvim_err_write("not found command: args=" .. vim.inspect(args) .. "\n")
+    return vim.api.nvim_err_write("not found command: cmd=" .. cmd_name .. "\n")
   end
 
-  local f = function()
-    return cmd(args)
-  end
-  local ok, result = xpcall(f, debug.traceback)
+  local ok, result = xpcall(cmd, debug.traceback)
   if not ok then
     error(result)
   end
