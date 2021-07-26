a06c52a0ce6b7d7a55001448ef69e6728f5c8752
notomo <notomo.motono@gmail.com>
2021-07-27 08:51:13 +0900

    Refactor tests

--- spec/lua/gesture/init_spec.lua
+++ spec/lua/gesture/init_spec.lua
@@ -1,12 +1,12 @@
 local helper = require("gesture.lib.testlib.helper")
 local gesture = helper.require("gesture")
 
-describe("gesture.nvim", function()
+describe("gesture.register()", function()
 
   before_each(helper.before_each)
   after_each(helper.after_each)
 
-  it("can register and execute a global gesture", function()
+  it("can register a global gesture", function()
     gesture.register({inputs = {gesture.down(), gesture.up()}, action = "normal! gg"})
 
     helper.set_lines([[
@@ -27,7 +27,7 @@ foo]])
     assert.current_line("hoge")
   end)
 
-  it("can register and execute a buffer local gesture", function()
+  it("can register a buffer local gesture", function()
     gesture.register({
       inputs = {gesture.right(), gesture.left()},
       action = "normal! $",
@@ -53,7 +53,7 @@ hoge         foo
   it("can use function as action", function()
     gesture.register({
       inputs = {gesture.down(), gesture.up()},
-      action = function(param)
+      action = function()
         vim.cmd("normal! gg")
       end,
     })
@@ -96,7 +96,7 @@ foo]])
     assert.equals(ctx.last_position[2], 21)
   end)
 
-  it("can execute a global gesture with matched buffer local gesture", function()
+  it("can register a global and buffer local gesture", function()
     gesture.register({
       inputs = {gesture.right({min_length = 10})},
       action = "normal! $",
@@ -117,22 +117,7 @@ bar]])
     assert.current_word("bar")
   end)
 
-  it("can register and execute a nowait gesture", function()
-    gesture.register({inputs = {gesture.right()}, action = "normal! $", nowait = true})
-
-    helper.set_lines([[
-hoge         foo
-]])
-
-    gesture.draw()
-    vim.cmd("normal! 10l")
-    gesture.draw()
-
-    assert.window_count(1)
-    assert.current_word("foo")
-  end)
-
-  it("can register and execute a nowait buffer local gesture", function()
+  it("can register a nowait buffer local gesture", function()
     gesture.register({inputs = {gesture.right()}, action = "normal! $", nowait = true, buffer = "%"})
     gesture.register({inputs = {gesture.right()}, action = "normal! 0", nowait = true})
     gesture.register({
@@ -151,24 +136,6 @@ hoge         foo
 
     assert.window_count(1)
     assert.current_word("foo")
-  end)
-
-  it("does nothing for non matched gesture", function()
-    gesture.register({inputs = {gesture.down(), gesture.up()}, action = "normal! G"})
-
-    helper.set_lines([[
-hoge
-
-
-foo]])
-
-    gesture.draw()
-    vim.cmd("normal! 10j")
-    gesture.draw()
-    gesture.finish()
-
-    assert.window_count(1)
-    assert.current_line("hoge")
   end)
 
   it("can register a gesture with max length", function()
@@ -201,77 +168,6 @@ hoge         foo
 
     assert.window_count(1)
     assert.current_word("hoge")
-  end)
-
-  it("shows input gestures", function()
-    gesture.draw()
-    vim.cmd("normal! 10l")
-    gesture.draw()
-    vim.cmd("normal! 10j")
-    gesture.draw()
-    vim.cmd("normal! 10h")
-    gesture.draw()
-    vim.cmd("normal! 10k")
-    gesture.draw()
-
-    assert.shown_in_view("RIGHT")
-    assert.shown_in_view("DOWN")
-    assert.shown_in_view("LEFT")
-    assert.shown_in_view("UP")
-  end)
-
-  it("shows matched gesture name", function()
-    gesture.register({name = "to bottom", inputs = {gesture.down()}, action = "normal! G"})
-    gesture.register({
-      name = "to top",
-      inputs = {gesture.down(), gesture.up()},
-      action = "normal! gg",
-    })
-
-    gesture.draw()
-    vim.cmd("normal! 10j")
-    gesture.draw()
-
-    assert.shown_in_view("DOWN")
-    assert.shown_in_view("to bottom")
-
-    vim.cmd("normal! 10k")
-    gesture.draw()
-
-    assert.shown_in_view("UP")
-    assert.shown_in_view("to top")
-  end)
-
-  it("raises no error with many gestures", function()
-    for _ = 0, 100, 1 do
-      gesture.draw()
-      vim.cmd("normal! 10j")
-      gesture.draw()
-      vim.cmd("normal! 10k")
-    end
-    assert.shown_in_view("UP")
-  end)
-
-  it("avoid creating multiple gesture state", function()
-    vim.cmd("tabedit")
-    gesture.draw()
-    vim.cmd("noautocmd tabprevious")
-
-    gesture.draw()
-    vim.cmd("tabnext")
-
-    assert.window_count(1)
-  end)
-
-  it("reset scroll on scrolled", function()
-    gesture.draw()
-    vim.cmd("normal! G")
-    vim.cmd("normal! zz")
-
-    -- NOTE: zz may not fire callback in headless mode.
-    vim.cmd("redraw")
-
-    assert.window_first_row(1)
   end)
 
   it("overwrites the same gesture", function()
@@ -312,6 +208,142 @@ hoge         foo
     assert.current_word("foo")
   end)
 
+end)
+
+describe("gesture.draw()", function()
+
+  before_each(helper.before_each)
+  after_each(helper.after_each)
+
+  it("shows input gestures", function()
+    gesture.draw()
+    vim.cmd("normal! 10l")
+    gesture.draw()
+    vim.cmd("normal! 10j")
+    gesture.draw()
+    vim.cmd("normal! 10h")
+    gesture.draw()
+    vim.cmd("normal! 10k")
+    gesture.draw()
+
+    assert.shown_in_view("RIGHT")
+    assert.shown_in_view("DOWN")
+    assert.shown_in_view("LEFT")
+    assert.shown_in_view("UP")
+  end)
+
+  it("shows matched gesture name", function()
+    gesture.register({name = "to bottom", inputs = {gesture.down()}, action = "normal! G"})
+    gesture.register({
+      name = "to top",
+      inputs = {gesture.down(), gesture.up()},
+      action = "normal! gg",
+    })
+
+    gesture.draw()
+    vim.cmd("normal! 10j")
+    gesture.draw()
+
+    assert.shown_in_view("DOWN")
+    assert.shown_in_view("to bottom")
+
+    vim.cmd("normal! 10k")
+    gesture.draw()
+
+    assert.shown_in_view("UP")
+    assert.shown_in_view("to top")
+  end)
+
+  it("executes a nowait gesture if it is matched", function()
+    gesture.register({inputs = {gesture.right()}, action = "normal! $", nowait = true})
+
+    helper.set_lines([[
+hoge         foo
+]])
+
+    gesture.draw()
+    vim.cmd("normal! 10l")
+    gesture.draw()
+
+    assert.window_count(1)
+    assert.current_word("foo")
+  end)
+
+  it("resets scroll on scrolled", function()
+    gesture.draw()
+    vim.cmd("normal! G")
+    vim.cmd("normal! zz")
+
+    -- NOTE: zz may not fire callback in headless mode.
+    vim.cmd("redraw")
+
+    assert.window_first_row(1)
+  end)
+
+  it("raises no error with many gestures", function()
+    for _ = 0, 100, 1 do
+      gesture.draw()
+      vim.cmd("normal! 10j")
+      gesture.draw()
+      vim.cmd("normal! 10k")
+    end
+    assert.shown_in_view("UP")
+  end)
+
+  it("does not create multiple gesture state", function()
+    vim.cmd("tabedit")
+    gesture.draw()
+    vim.cmd("noautocmd tabprevious")
+
+    gesture.draw()
+    vim.cmd("tabnext")
+
+    assert.window_count(1)
+  end)
+
+end)
+
+describe("gesture.finish()", function()
+
+  before_each(helper.before_each)
+  after_each(helper.after_each)
+
+  it("does nothing for non matched gesture", function()
+    gesture.register({inputs = {gesture.down(), gesture.up()}, action = "normal! G"})
+
+    helper.set_lines([[
+hoge
+
+
+foo]])
+
+    gesture.draw()
+    vim.cmd("normal! 10j")
+    gesture.draw()
+    gesture.finish()
+
+    assert.window_count(1)
+    assert.current_line("hoge")
+  end)
+
+  it("shows raw error", function()
+    gesture.register({inputs = {gesture.down()}, action = "invalid_command"})
+
+    gesture.draw()
+    vim.cmd("normal! 10j")
+    gesture.draw()
+    gesture.finish()
+
+    assert.exists_message("%[gesture%] Vim:E492: Not an editor command: invalid_command")
+  end)
+
+end)
+
+describe("gesture.cancel()", function()
+
+  before_each(helper.before_each)
+  after_each(helper.after_each)
+
   it("can cancel gesture", function()
     gesture.register({inputs = {gesture.down(), gesture.up()}, action = "normal! gg"})
 
@@ -333,15 +365,32 @@ foo]])
     assert.current_line("foo")
   end)
 
-  it("shows raw error", function()
-    gesture.register({inputs = {gesture.down()}, action = "invalid_command"})
+end)
+
+describe("gesture.clear()", function()
+
+  before_each(helper.before_each)
+  after_each(helper.after_each)
+
+  it("can clear gestures", function()
+    gesture.register({inputs = {gesture.down(), gesture.up()}, action = "normal! gg"})
+    gesture.clear()
+
+    helper.set_lines([[
+hoge
+
+
+foo]])
+    vim.cmd("normal! G")
 
     gesture.draw()
     vim.cmd("normal! 10j")
     gesture.draw()
+    vim.cmd("normal! 10k")
+    gesture.draw()
     gesture.finish()
 
-    assert.exists_message("%[gesture%] Vim:E492: Not an editor command: invalid_command")
+    assert.current_line("foo")
   end)
 
 end)
