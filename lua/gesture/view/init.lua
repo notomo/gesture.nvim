510411f408e4243fe0dafe6d09625c666abfb7c7
notomo <notomo.motono@gmail.com>
2023-03-27 10:17:22 +0900

    Fix highlight

--- lua/gesture/view/init.lua
+++ lua/gesture/view/init.lua
@@ -56,22 +56,22 @@ local setup_highlight_groups = function()
   local blend = 0
   return {
     highlightlib.define("GestureLine", {
-      bg = vim.api.nvim_get_hl(0, { name = "Statement" }).foreground or "#a8d2eb",
+      bg = vim.api.nvim_get_hl(0, { name = "Statement" }).fg or "#a8d2eb",
       blend = 25,
     }),
     highlightlib.define("GestureInput", {
-      fg = vim.api.nvim_get_hl(0, { name = "NormalFloat" }).foreground or "#fffeeb",
-      bg = vim.api.nvim_get_hl(0, { name = "NormalFloat" }).background or "#3a4b5c",
+      fg = vim.api.nvim_get_hl(0, { name = "NormalFloat" }).fg or "#fffeeb",
+      bg = vim.api.nvim_get_hl(0, { name = "NormalFloat" }).bg or "#3a4b5c",
       blend = blend,
       bold = true,
     }),
     highlightlib.define("GestureInputNotMatched", {
-      fg = vim.api.nvim_get_hl(0, { name = "Comment" }).foreground or "#8d9eb2",
-      bg = vim.api.nvim_get_hl(0, { name = "NormalFloat" }).background or "#3a4b5c",
+      fg = vim.api.nvim_get_hl(0, { name = "Comment" }).fg or "#8d9eb2",
+      bg = vim.api.nvim_get_hl(0, { name = "NormalFloat" }).bg or "#3a4b5c",
       blend = blend,
     }),
     highlightlib.define("GestureActionLabel", {
-      fg = vim.api.nvim_get_hl(0, { name = "Statement" }).foreground or "#a8d2eb",
+      fg = vim.api.nvim_get_hl(0, { name = "Statement" }).fg or "#a8d2eb",
       blend = blend,
       bold = true,
     }),
