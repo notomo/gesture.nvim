5f2ea4764509ba83df260243e1dbce1fa06437f7
notomo <notomo.motono@gmail.com>
2023-06-23 22:17:51 +0900

    Add match, can_match tests

--- spec/lua/gesture/init_spec.lua
+++ spec/lua/gesture/init_spec.lua
@@ -5,7 +5,7 @@ describe("gesture.register()", function()
   before_each(helper.before_each)
   after_each(helper.after_each)
 
-  it("can register a global gesture", function()
+  it("can register a global gesture with inputs", function()
     gesture.register({ inputs = { gesture.down(), gesture.up() }, action = "normal! gg" })
 
     helper.set_lines([[
@@ -26,7 +26,7 @@ foo]])
     assert.current_line("hoge")
   end)
 
-  it("can register a buffer local gesture", function()
+  it("can register a buffer local gesture with inputs", function()
     gesture.register({
       inputs = { gesture.right(), gesture.left() },
       action = "normal! $",
@@ -47,6 +47,119 @@ hoge         foo
 
     assert.window_count(1)
     assert.cursor_word("foo")
+  end)
+
+  it("can register a global gesture with match", function()
+    gesture.register({
+      match = function(ctx)
+        local last_input = ctx.inputs[#ctx.inputs]
+        return last_input and last_input.direction == "DOWN"
+      end,
+      action = "normal! gg",
+    })
+
+    helper.set_lines([[
+hoge
+
+
+foo]])
+    vim.cmd.normal({ args = { "G" }, bang = true })
+
+    gesture.draw()
+    vim.cmd.normal({ args = { "10l" }, bang = true })
+    gesture.draw()
+    vim.cmd.normal({ args = { "10j" }, bang = true })
+    gesture.draw()
+    gesture.finish()
+
+    assert.window_count(1)
+    assert.current_line("hoge")
+  end)
+
+  it("can register a buffer local gesture with match", function()
+    local called = false
+    gesture.register({
+      match = function(ctx)
+        local input1 = ctx.inputs[1]
+        local input2 = ctx.inputs[2]
+        return input1 and input2 and input1.direction == "RIGHT" and input2.direction == "LEFT"
+      end,
+      action = function()
+        called = true
+      end,
+      buffer = "%",
+    })
+    gesture.register({
+      match = function(ctx)
+        local input1 = ctx.inputs[1]
+        local input2 = ctx.inputs[2]
+        return input1 and input2 and input1.direction == "RIGHT" and input2.direction == "LEFT"
+      end,
+      action = function()
+        error("should not be called")
+      end,
+    })
+
+    gesture.draw()
+    vim.cmd.normal({ args = { "10l" }, bang = true })
+    gesture.draw()
+    vim.cmd.normal({ args = { "10h" }, bang = true })
+    gesture.draw()
+    gesture.finish()
+
+    assert.window_count(1)
+    assert.is_true(called)
+  end)
+
+  it("can register a nowait gesture with match", function()
+    local called = false
+    gesture.register({
+      match = function(ctx)
+        local input1 = ctx.inputs[1]
+        local input2 = ctx.inputs[2]
+        return input1 and input2 and input1.direction == "RIGHT" and input2.direction == "LEFT"
+      end,
+      action = function()
+        called = true
+      end,
+      nowait = true,
+    })
+
+    gesture.draw()
+    vim.cmd.normal({ args = { "10l" }, bang = true })
+    gesture.draw()
+    vim.cmd.normal({ args = { "10h" }, bang = true })
+    gesture.draw()
+
+    assert.window_count(1)
+    assert.is_true(called)
+  end)
+
+  it("can register a gesture with match, can_match", function()
+    local called = false
+    gesture.register({
+      match = function(ctx)
+        local input2 = ctx.inputs[2]
+        return input2 and input2.direction == "LEFT"
+      end,
+      can_match = function(ctx)
+        local input1 = ctx.inputs[1]
+        return input1 and input1.direction == "RIGHT"
+      end,
+      action = function()
+        called = true
+      end,
+    })
+
+    gesture.draw()
+    vim.cmd.normal({ args = { "10l" }, bang = true })
+    gesture.draw()
+    vim.cmd.normal({ args = { "10h" }, bang = true })
+    gesture.draw()
+    gesture.finish()
+
+    assert.window_count(1)
+    assert.is_true(called)
   end)
 
   it("can use function as action", function()
