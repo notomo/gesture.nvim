5124a05debd62326cc899a35a09862a675d7f3da
notomo <notomo.motono@gmail.com>
2024-01-09 21:10:04 +0900

    Update vendor

--- lua/gesture/vendor/misclib/highlight.lua
+++ lua/gesture/vendor/misclib/highlight.lua
@@ -1,5 +1,9 @@
 local M = {}
 
+--- Defines a default highlight group.
+--- @param hl_group string: highlight group name
+--- @param attributes table: |nvim_set_hl| val parameter
+--- @return string # hl_group
 function M.define(hl_group, attributes)
   local new_attributes = { default = true }
   for key, value in pairs(attributes) do
@@ -11,6 +15,10 @@ function M.define(hl_group, attributes)
   return hl_group
 end
 
+--- Makes a highlight group link.
+--- @param hl_group string: highlight group name
+--- @param to string: highlight group name to link |:hl-link|
+--- @return string # hl_group
 function M.link(hl_group, to)
   vim.api.nvim_set_hl(0, hl_group, {
     link = to,
